c44c002cb4ad4db020e8479b26aec9a3e69cd607
Bejiin Jaden <bejiinjaden1981@gmail.com>
2024-04-06 21:12:38 +0530

    update4

--- src/main.js
+++ src/main.js
@@ -38,14 +38,33 @@ let products = [
 
 ];
 
-let producthtml;
-
-products.forEach(element =>{
 
 
+let producthtml = "";
+
+products.forEach((element) =>{
+
+producthtml+= `<div class="product">
+
+               
+<img src="${element.image}" class="plant_img">
+
+
+<div class="plant_name">
+<p>I Dont Know</p>
+</div>
+
+<div class="plant_price">
+<p>Price : $69420</p>
+</div>
+
+
+</div>`
 
 
 
-})
+});
+
+document.querySelector('.plants_display').innerHTML = producthtml;
 
 
