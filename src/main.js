edaa11ed3e5676ca2f2b8a18ec0d385fa8ae5677
Bejiin Jaden <bejiinjaden1981@gmail.com>
2024-04-06 19:57:33 +0530

    update3

---
+++ src/main.js
@@ -0,0 +1,51 @@
+let products = [
+
+ {
+    name:"I Dont Know",
+
+    image:"Images/plant.png",
+
+    price : 69420
+
+ },
+
+ {
+    name:"I Dont Know",
+
+    image:"Images/plant.png",
+
+    price : 69420
+
+ },
+
+ {
+    name:"I Dont Know",
+
+    image:"Images/plant.png",
+
+    price : 69420
+
+ },
+
+ {
+    name:"I Dont Know",
+
+    image:"Images/plant.png",
+
+    price : 69420
+
+ },
+
+];
+
+let producthtml;
+
+products.forEach(element =>{
+
+
+
+
+
+})
+
+
